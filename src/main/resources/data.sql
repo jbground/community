853418839ed5caf5ddc05b100a1dd47aaeede654
JeongJiSang <jsj00203@gmail.com>
2023-08-08 10:30:12 +0900

    data.sql 더미데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,26 @@ INSERT INTO MEMBER VALUES ('abc123', '홍길동', 'abc123', current_timestamp, '
 
 -- BOARD DATA
 INSERT INTO BOARD VALUES (1, '테스트입니다1', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (2, '테스트입니다2', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (3, '테스트입니다3', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (4, '테스트입니다4', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (5, '테스트입니다5', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (6, '테스트입니다6', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (7, '테스트입니다7', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (8, '테스트입니다8', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (9, '테스트입니다9', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (10, '테스트입니다10', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (11, '테스트입니다11', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (12, '테스트입니다12', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (13, '테스트입니다13', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (14, '테스트입니다14', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (15, '테스트입니다15', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (16, '테스트입니다16', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (17, '테스트입니다17', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (18, '테스트입니다18', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (19, '테스트입니다19', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (20, '테스트입니다20', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (21, '테스트입니다21', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (22, '테스트입니다22', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (23, '테스트입니다23', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
+INSERT INTO BOARD VALUES (24, '테스트입니다24', 'abc123', current_timestamp, current_timestamp, '게시물 등록 테스트 입니다.', '127.0.0.1');
