a860075d4d27de0f2411181306fad971ffa4f99a
jakyunglee <jakyunglee@DESKTOP-RQGQG0L>
2023-09-04 14:11:20 +0900

    2023/09/04 게시판 - rest url

--- src/main/java/com/jbground/community/web/board/BoardController.java
+++ src/main/java/com/jbground/community/web/board/BoardController.java
@@ -1,12 +1,16 @@
 package com.jbground.community.web.board;
 
 import com.jbground.community.model.Board;
+import com.jbground.community.model.common.ResponseStatus;
 import com.jbground.community.web.board.dao.BoardDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -33,4 +37,39 @@ public class BoardController {
 
         return "thymeleaf/board/board_list";
     }
+    
+    @ResponseBody
+    @RequestMapping(value= "/board/insert", method = RequestMethod.POST)
+    public ResponseStatus insertBoard(HttpServletRequest request, ModelMap model, @ModelAttribute Board board) throws Exception{
+		
+    	String msg = "";    
+    	ResponseStatus status = new ResponseStatus(0, msg);
+    	
+    	return status;
+    	
+    }
+    
+    
+    @ResponseBody
+    @RequestMapping(value = "/board/update", method = RequestMethod.POST)
+    public ResponseStatus updateBoard(HttpServletRequest request, ModelMap model, @ModelAttribute Board board) throws Exception{
+		
+    	String msg = "";    
+    	ResponseStatus status = new ResponseStatus(0, msg);
+    	
+    	return status;	
+    	
+    }
+    
+    @ResponseBody
+    @RequestMapping(value = "/board/delete", method = RequestMethod.POST)
+    public ResponseStatus deleteBoard(HttpServletRequest request, ModelMap model, @ModelAttribute Board board) throws Exception{
+		
+    	String msg = "";    
+    	ResponseStatus status = new ResponseStatus(0, msg);
+    	
+    	return status;	
+    	
+    }
+    
 }
